8be557af04fecde7d29f26320db75b53efed1380
louhichi-rihem <129398762+louhichi-rihem@users.noreply.github.com>
2023-03-30 22:13:53 +0200

    Add files via upload

---
+++ game3/game/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace game
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblStatues = new System.Windows.Forms.Label();
+            this.lblTimeLeft = new System.Windows.Forms.Label();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblStatues
+            // 
+            this.lblStatues.AutoSize = true;
+            this.lblStatues.Location = new System.Drawing.Point(328, 101);
+            this.lblStatues.Name = "lblStatues";
+            this.lblStatues.Size = new System.Drawing.Size(136, 20);
+            this.lblStatues.TabIndex = 0;
+            this.lblStatues.Text = "Match or MisMatch";
+            this.lblStatues.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblTimeLeft
+            // 
+            this.lblTimeLeft.AutoSize = true;
+            this.lblTimeLeft.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTimeLeft.Location = new System.Drawing.Point(326, 136);
+            this.lblTimeLeft.Name = "lblTimeLeft";
+            this.lblTimeLeft.Size = new System.Drawing.Size(137, 28);
+            this.lblTimeLeft.TabIndex = 1;
+            this.lblTimeLeft.Text = "Time Left: 30";
+            this.lblTimeLeft.Click += new System.EventHandler(this.lblTimeLeft_Click);
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnRestart.Location = new System.Drawing.Point(328, 21);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(135, 53);
+            this.btnRestart.TabIndex = 2;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.RestartGameEvent);
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Interval = 1000;
+            this.GameTimer.Tick += new System.EventHandler(this.TamerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 353);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.lblTimeLeft);
+            this.Controls.Add(this.lblStatues);
+            this.Name = "Form1";
+            this.Text = "Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lblStatues;
+        private Label lblTimeLeft;
+        private Button btnRestart;
+        private System.Windows.Forms.Timer GameTimer;
+    }
+}
